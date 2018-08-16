5111e39ec9b580d227adf34a1563b46db635521d
deepak <deepakbhimavarapu@gmail.com>
2018-08-16 10:23:43 +0530

    " -> 0 of 8 passed. pylint: 7.91/10 "

--- my_practice/poker_version2/poker.py
+++ my_practice/poker_version2/poker.py
@@ -142,8 +142,9 @@ if __name__ == "__main__":
         line = input()
         ha = line.split(" ")
         HANDS.append(ha)
+    print(HANDS)
     # test the poker function to see how it works
-    print(' '.join(poker(HANDS)))
+    # print(' '.join(poker(HANDS)))
 
 # kind(['AD','AD','TD','JD','QD'])
 # print(is_straight(['AD','KD','TD','JD','QD']))

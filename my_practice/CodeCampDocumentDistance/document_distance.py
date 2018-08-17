70276624d9ecbacfde0a434632b67239f4bab68c
deepak <deepakbhimavarapu@gmail.com>
2018-08-17 11:49:42 +0530

    " -> 3 of 3 passed. pylint: 9.02/10 "

--- my_practice/CodeCampDocumentDistance/document_distance.py
+++ my_practice/CodeCampDocumentDistance/document_distance.py
@@ -4,11 +4,11 @@
 import math
 def build_dict(dict1):
     dictionary = {}
-    dict1 = ''.join(e for e in dict1 if e.isalpha() or e==' ')
+    dict1 = ''.join(e for e in dict1 if e.isalpha() or e == ' ')
     list_words = dict1.lower().split(" ")
     stop_words = load_stopwords("stopwords.txt")
     for word in list_words:
-        if word not in stop_words and len(word)>0: 
+        if word not in stop_words and len(word) > 0:
             if word in dictionary:
                 dictionary[word] += 1
             else:
@@ -19,7 +19,7 @@ def built_common_dict(dict1, dict2):
     common_dict = {}
     for key in dict1:
         if key in dict2:
-            common_dict[key] = [dict1[key] , dict2[key]]
+            common_dict[key] = [dict1[key], dict2[key]]
     return common_dict
 
 def similarity(dict1, dict2):
@@ -35,19 +35,18 @@ def similarity(dict1, dict2):
     common_dict = built_common_dict(dictionary1, dictionary2)
     for key in dictionary1:
         if key not in common_dict:
-            common_dict[key] = [dictionary1[key],0]
+            common_dict[key] = [dictionary1[key], 0]
     for key in dictionary2:
         if key not in common_dict:
-            common_dict[key] = [0,dictionary2[key]]        
+            common_dict[key] = [0, dictionary2[key]]
     for value in common_dict.values():
         numerator += value[0] * value[1]
         denominator1 += value[0] ** 2
         denominator2 += value[1] ** 2
-    
     denominator = (math.sqrt(denominator1) * math.sqrt(denominator2))
     if denominator == 0:
         return 0.0
-    return (numerator / denominator )
+    return numerator / denominator
 
     
 
